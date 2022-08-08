0543c75b588560679e0bade7d406b15908ce3ada
StarryChen1102 <104817932+StarryChen1102@users.noreply.github.com>
2022-08-08 21:56:46 +0800

    Update AML_preprocessing.R

--- AML_preprocessing.R
+++ AML_preprocessing.R
@@ -1,64 +1,101 @@
-test.data.1 <- read.table("AML_D0/GSM3587923_AML1012-D0.dem.txt", sep="\t", header=TRUE)
-test_data.2 <- read.table("AML_D0/GSM3587925_AML210A-D0.dem.txt", sep="\t", header=TRUE)
-test_data.3 <- read.table("AML_D0/GSM3587927_AML314-D0.dem.txt", sep="\t", header=TRUE)
+test.data.0 <- read.table("AML_data/AML707B/GSM3587969_AML707B-D0.dem.txt", sep="\t", header=TRUE)
+test.data.18 <- read.table("AML_data/AML707B/GSM3587973_AML707B-D18.dem.txt", sep="\t", header=TRUE)
+test.data.41 <- read.table("AML_data/AML707B/GSM3587975_AML707B-D41.dem.txt", sep="\t", header=TRUE)
+test.data.97 <- read.table("AML_data/AML707B/GSM3587977_AML707B-D97.dem.txt", sep="\t", header=TRUE)
+test.data.113 <- read.table("AML_data/AML707B/GSM3587971_AML707B-D113.dem.txt", sep="\t", header=TRUE)
 
-test_data.4 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test_data.5 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test.data.6 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test_data.7 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test_data.8 <- read.table("AML_D0/GSM3587953_AML420B-D0.dem.txt", sep="\t", header=TRUE)
-test_data.9 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test.data.10 <- read.table("AML_D0/GSM3587969_AML707B-D0.dem.txt", sep="\t", header=TRUE)
-test.data.11 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test.data.12 <- read.table("AML_D0/GSM3587988_AML916-D0.dem.txt", sep="\t", header=TRUE)
-test_data.13 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test.data.14 <- read.table("AML_D0/GSM3587959_AML475-D0.dem.txt", sep="\t", header=TRUE)
-test_data.15 <- read.table("AML-029-08-1E_data.csv", sep="\t", header=TRUE)
-test.data.16 <- read.table("AML_D0/GSM3587990_AML921A-D0.dem.txt", sep="\t", header=TRUE)
+test.data.0.anno <- read.table("AML_data/AML707B/GSM3587970_AML707B-D0.anno.txt", sep="\t", header=TRUE)
+test.data.18.anno <- read.table("AML_data/AML707B/GSM3587974_AML707B-D18.anno.txt", sep="\t", header=TRUE)
+test.data.41.anno <- read.table("AML_data/AML707B/GSM3587976_AML707B-D41.anno.txt", sep="\t", header=TRUE)
+test.data.97.anno <- read.table("AML_data/AML707B/GSM3587978_AML707B-D97.anno.txt", sep="\t", header=TRUE)
+test.data.113.anno <- read.table("AML_data/AML707B/GSM3587972_AML707B-D113.anno.txt", sep="\t", header=TRUE)
 
-test.data.1.anno <- read.table("AML_D0_anno/GSM3587924_AML1012-D0.anno.txt", sep="\t", header=TRUE)
-test.data.8.anno <- read.table("AML_D0_anno/GSM3587954_AML420B-D0.anno.txt", sep="\t", header=TRUE)
-test.data.10.anno <- read.table("AML_D0_anno/GSM3587970_AML707B-D0.anno.txt", sep="\t", header=TRUE)
-test.data.12.anno <- read.table("AML_D0_anno/GSM3587989_AML916-D0.anno.txt", sep="\t", header=TRUE)
-test.data.14.anno <- read.table("AML_D0_anno/GSM3587960_AML475-D0.anno.txt", sep="\t", header=TRUE)
-test.data.16.anno <- read.table("AML_D0_anno/GSM3587991_AML921A-D0.anno.txt", sep="\t", header=TRUE)
+test.data.0 <- as.data.frame(t(test.data.0))
+colnames(test.data.0) <- test.data.0[1,]
+test.data.0 <- test.data.0[2:nrow(test.data.0),]
+test.data.0 <- apply(test.data.0, 2, as.numeric)
+test.data.0 <- as.data.frame(test.data.0)
 
-test.data.1 <- test.data.10
-test.data.1.anno <- test.data.10.anno
+test.data.18 <- as.data.frame(t(test.data.18))
+colnames(test.data.18) <- test.data.18[1,]
+test.data.18 <- test.data.18[2:nrow(test.data.18),]
+test.data.18 <- apply(test.data.18, 2, as.numeric)
+test.data.18 <- as.data.frame(test.data.18)
 
-test.data.1 <- as.data.frame(t(test.data.1))
-colnames(test.data.1) <- test.data.1[1,]
-test.data.1 <- test.data.1[2:nrow(test.data.1),]
-test.data.1.new <- apply(test.data.1, 2, as.numeric)
-test.data.1.new <- as.data.frame(test.data.1.new)
+test.data.41 <- as.data.frame(t(test.data.41))
+colnames(test.data.41) <- test.data.41[1,]
+test.data.41 <- test.data.41[2:nrow(test.data.41),]
+test.data.41 <- apply(test.data.41, 2, as.numeric)
+test.data.41 <- as.data.frame(test.data.41)
 
-write.table(test.data.1.new, file='GSM3587969_AML707B-D0_data.csv', quote=FALSE, sep='\t', col.names = NA)
+test.data.97 <- as.data.frame(t(test.data.97))
+colnames(test.data.97) <- test.data.97[1,]
+test.data.97 <- test.data.97[2:nrow(test.data.97),]
+test.data.97 <- apply(test.data.97, 2, as.numeric)
+test.data.97 <- as.data.frame(test.data.97)
 
-test.label.1 <- test.data.1.anno$CellType
-write.table(t(test.label.1), file='GSM3587969_AML707B-D0_info.csv', quote=FALSE, sep='\t', col.names = NA)
+test.data.113 <- as.data.frame(t(test.data.113))
+colnames(test.data.113) <- test.data.113[1,]
+test.data.113 <- test.data.113[2:nrow(test.data.113),]
+test.data.113 <- apply(test.data.113, 2, as.numeric)
+test.data.113 <- as.data.frame(test.data.113)
 
-unique(test.data.1.anno$CellType)
-test.label.1[test.label.1 == "GMP" | test.label.1 == "GMP-like"] <- "GMP"
-test.label.1[test.label.1 == "Prog" | test.label.1 == "Prog-like"] <- "Prog"
-test.label.1[test.label.1 == "ProMono" | test.label.1 == "ProMono-like"] <- "ProMono"
-test.label.1[test.label.1 == "cDC" | test.label.1 == "cDC-like"] <- "cDC"
-test.label.1[test.label.1 == "Mono" | test.label.1 == "Mono-like"] <- "Mono"
-test.label.1[test.label.1 == "HSC" | test.label.1 == "HSC-like"] <- "HSC"
-test.label.1[test.label.1 == "T"] <- "T"
-test.label.1[test.label.1 == "CTL"] <- "CTL"
-test.label.1[test.label.1 == "Plasma"] <- "Plasma"
-test.label.1[test.label.1 == "ProB"] <- "ProB"
-test.label.1[test.label.1 == "earlyEry"] <- "earlyEry"
-test.label.1[test.label.1 == "NK"] <- "NK"
-test.label.1[test.label.1 == "pDC"] <- "pDC"
+test.data.all <- rbind(test.data.0, test.data.18, test.data.41, test.data.97, test.data.113)
+test.data.all <- test.data.all[,which(colSums(test.data.all)>0)]
 
-write.table(t(test.label.1), file='GSM3587969_AML707B-D0_info_new.csv', quote=FALSE, sep='\t', col.names = NA)
+write.table(test.data.all, file='AML_data/AML707B/all/AML707B-all_data.csv', quote=FALSE, sep='\t', col.names = NA)
 
-test.name <- unique(test.label.1)
+test.data.0.anno <- test.data.0.anno$CellType
+test.data.18.anno <- test.data.18.anno$CellType
+test.data.41.anno <- test.data.41.anno$CellType
+test.data.97.anno <- test.data.97.anno$CellType
+test.data.113.anno <- test.data.113.anno$CellType
+
+test.data.0.size <- nrow(test.data.0)
+test.data.18.size <- nrow(test.data.18)
+test.data.41.size <- nrow(test.data.41)
+test.data.97.size <- nrow(test.data.97)
+test.data.113.size <- nrow(test.data.113)
+
+test.data.all.anno <- c(test.data.0.anno, test.data.18.anno, test.data.41.anno, test.data.97.anno, test.data.113.anno)
+
+write.table(t(test.data.all.anno), file='AML_data/AML707B/all/AML707B-all_celltype.csv', quote=FALSE, sep='\t', col.names = NA)
+
+unique(test.data.all.anno)
+
+test.data.all.anno[test.data.all.anno == "GMP" | test.data.all.anno == "GMP-like"] <- "GMP"
+test.data.all.anno[test.data.all.anno == "Prog" | test.data.all.anno == "Prog-like"] <- "Prog"
+test.data.all.anno[test.data.all.anno == "ProMono" | test.data.all.anno == "ProMono-like"] <- "ProMono"
+test.data.all.anno[test.data.all.anno == "cDC" | test.data.all.anno == "cDC-like"] <- "cDC"
+test.data.all.anno[test.data.all.anno == "Mono" | test.data.all.anno == "Mono-like"] <- "Mono"
+test.data.all.anno[test.data.all.anno == "HSC" | test.data.all.anno == "HSC-like"] <- "HSC"
+test.data.all.anno[test.data.all.anno == "T"] <- "T"
+test.data.all.anno[test.data.all.anno == "CTL"] <- "CTL"
+test.data.all.anno[test.data.all.anno == "Plasma"] <- "Plasma"
+test.data.all.anno[test.data.all.anno == "ProB"] <- "ProB"
+test.data.all.anno[test.data.all.anno == "earlyEry"] <- "earlyEry"
+test.data.all.anno[test.data.all.anno == "lateEry"] <- "lateEry"
+test.data.all.anno[test.data.all.anno == "NK"] <- "NK"
+test.data.all.anno[test.data.all.anno == "pDC"] <- "pDC"
+
+write.table(t(test.data.all.anno), file='AML_data/AML707B/all/AML707B-all_celltype_merge.csv', quote=FALSE, sep='\t', col.names = NA)
+
+test.name <- unique(test.data.all.anno)
 test.name <- as.data.frame(test.name)
 rownames(test.name) <- c(1:nrow(test.name))
 test.name$number <- c(1:nrow(test.name))
 
-for (i in 1:nrow(test.name)){test.label.1[test.label.1 == test.name[i,1]] <- test.name[i,2]}
+for (i in 1:nrow(test.name)){test.data.all.anno[test.data.all.anno == test.name[i,1]] <- (test.name[i,2] - 1)}
 
-write.table(t(test.label.1), file='GSM3587969_AML707B-D0_info_number.csv', quote=FALSE, sep='\t', col.names = NA)
+write.table(t(test.data.all.anno), file='AML_data/AML707B/all/AML707B-all_celltype_number.csv', quote=FALSE, sep='\t', col.names = NA)
+
+test.data.all.sample <- test.data.all.anno
+test.data.all.sample[1:1586] <- "D0"
+test.data.all.sample[1587:3259] <- "D18"
+test.data.all.sample[3260:3646] <- "D41"
+test.data.all.sample[3647:3730] <- "D97"
+test.data.all.sample[3731:4438] <- "D113"
+
+write.table(t(test.data.all.sample), file='AML_data/AML707B/all/AML707B-all_celltype_sample.csv', quote=FALSE, sep='\t', col.names = NA)
+
+write.table(t(colnames(test.data.all)), file='AML_data/AML707B/all/AML707B-all_genes.csv', quote=FALSE, sep='\t', col.names = NA)
